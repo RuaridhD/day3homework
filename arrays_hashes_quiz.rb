87eecf18ffca70e0181a9e3656abdb4276006f25
Ruaridh <ruaridhdunbar@gmail.com>
2018-03-28 17:23:20 +0100

    finish ex2  question 9

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -117,7 +117,10 @@ p users["Erik"][:lottery_numbers] << 7
 users["Erik"][:home_town] = "Edinburgh"
 p users["Erik"][:home_town]
 # 9. Add a pet dog to Erik called "Fluffy"
+users["Erik"][:pets] << [:name => "Fluffy", :species => "dog"]
+p users ["Erik"][:pets]
 # 10. Add another person to the users hash
+
 
 
 ## Exercise C
