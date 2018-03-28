3599cc963765e661e9be31b98ca19f6a121772d3
Ruaridh <ruaridhdunbar@gmail.com>
2018-03-28 17:18:12 +0100

    finish ex2 q8

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -110,9 +110,12 @@ p users["Avril"][:pets][0][:species]
 # 5. Get the smallest of Erik's lottery numbers
 p users["Erik"][:lottery_numbers].min
 # 6. Return an array of Avril's lottery numbers that are even
-
+p users["Avril"][:lottery_numbers].select(&:even?)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+p users["Erik"][:lottery_numbers] << 7
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = "Edinburgh"
+p users["Erik"][:home_town]
 # 9. Add a pet dog to Erik called "Fluffy"
 # 10. Add another person to the users hash
 
